7cea68416027bc8dfb38122c0cf3034f96b380a0
rafaelovna <aikanush777@mail.ru>
2022-12-19 15:57:11 +0400

    Сделано.

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main {
         System.out.println("ЗАДАНИЕ 1");
         printLeapYear(2020);
         System.out.println("ЗАДАНИЕ 2");
-        printParameters(1,2022);
+        printParameters(3,2022);
         System.out.println("ЗАДАНИЕ 3");
         printCountDays(95);
     }
     //ЗАДАНИЕ 1
-    public static boolean countDate(int year) {
+    public static boolean isLeapYear(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
     public static void printLeapYear(int year) {
-        if (countDate(year)) {
+        if (isLeapYear(year)) {
             System.out.println(year + " год — високосный год");
         } else {
             System.out.println(year + " год — невисокосный год");
@@ -22,7 +22,7 @@ public class Main {
     }
     // ЗАДАНИЕ 2
     public static void printParameters(int osMobile, int dateRelease) {
-        System.out.println(findParametersDate(dateRelease) + " версию приложения для " + findParameters(osMobile));
+        System.out.println(findParametersDate(dateRelease) + " версию приложения для: " + findParameters(osMobile));
     }
     public static String findParameters(int osMobile) {
         if (osMobile == 0) {
@@ -42,7 +42,7 @@ public class Main {
         }
     }
     //ЗАДАНИЕ 3
-    public static int distance (int  deliveryDistance) {
+    public static int calculateDeliveryDays(int  deliveryDistance) {
         if ( deliveryDistance < 20) {
             return 1;
         } else if ( deliveryDistance <= 60) {
@@ -57,7 +57,7 @@ public class Main {
         if (deliveryDistance > 100){
         System.out.println("Свыше 100 км доставки нет.");
     } else {
-            System.out.println("Потребуется дней: " + distance(deliveryDistance));
+            System.out.println("Потребуется дней: " + calculateDeliveryDays(deliveryDistance));
         }
 
     }
